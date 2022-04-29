51c3e739388bd9f118a2344518c429f2f01aa125
Yuri Vesnin <uvesnin@gmail.com>
2022-04-29 03:15:52 +0300

    Refusing datepicker & fixed fonts & fixed logic

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3
 
 RUN pip install flask
+ADD app ./app
 
 ENTRYPOINT [ "sh", "-c" ]
-CMD ["flask run"]
+CMD ["flask run --host=0.0.0.0"]
--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ entries_pickle = "./entries.pickle"
 
 if os.path.exists(entries_pickle):
     try:
-        with open(entries_pickle) as entries_file:
+        with open(entries_pickle, "rb") as entries_file:
             entries = pickle.load(entries_file)
     except Exception:
         logging.exception("Can't import last data")
@@ -21,12 +21,13 @@ if os.path.exists(entries_pickle):
 def index():
     return flask.render_template("base.jinja2", entries=entries)
 
-@app.route("/", methods=["get"])
-def index():
+@app.route("/", methods=["post"])
+def push():
+    global entries
     entries.append(dict(**flask.request.form))
 
     try:
-        with open(entries_pickle, "w") as entries_file:
+        with open(entries_pickle, "wb") as entries_file:
             entries = pickle.dump(entries_file)
     except Exception:
         logging.exception("Can't write current data")
